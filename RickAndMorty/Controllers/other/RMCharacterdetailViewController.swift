b5388ef192e7e957921e819e8568528682fc3ac0
Chirag Arora <arora2002chirag@gmail.com>
2023-01-05 04:49:45 +0530

    CharacterDetailView

--- RickAndMorty/Controllers/other/RMCharacterdetailViewController.swift
+++ RickAndMorty/Controllers/other/RMCharacterdetailViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 /// Controller to show info about a single character
 final class RMCharacterdetailViewController: UIViewController {
     private let viewModel: RMCharacterDetailViewViewModel
+    
+    private let detailView = RMCharacterDetailView()
+    
+    //MARK: - Init
+    
+    
+    
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -25,8 +32,32 @@ final class RMCharacterdetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
-
-       
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(didTapShare)
+        )
+        addConstraints()
+   }
+  
+    
+    @objc
+    private func didTapShare() {
+        // share character info
+        
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        
+        
+        ])
     }
  
 
