7b0e0307703697698de62732dc28f2089765f54f
Chirag Arora <arora2002chirag@gmail.com>
2023-01-24 00:32:17 +0530

    SwiftUI Settings View

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -6,15 +6,22 @@
 //
 
 import UIKit
+import SwiftUI
 
+/// Controller to show various app options and settings
 final class RMSettingsViewController: UIViewController {
     
-    private let viewModel = RMSettingsViewViewModel(
-        cellViewModels: RMSettingsOption.allCases.compactMap({
-            return RMSettingsCellViewModel(type: $0)
-        })
+    
+    
+    private let settingsSwiftUIController = UIHostingController(
+        rootView: RMSettingsView(viewModel:  RMSettingsViewViewModel(
+            cellViewModels: RMSettingsOption.allCases.compactMap({
+                return RMSettingsCellViewModel(type: $0)
+                })
+            )
+        )
     )
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,10 +29,29 @@ final class RMSettingsViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         title = "Settings"
-
-     
+        addswiftUIController()
+        
     }
     
-
-
+    private func addswiftUIController() {
+        addChild(settingsSwiftUIController)
+        
+        settingsSwiftUIController.didMove(toParent: self)
+        
+        view.addSubview(settingsSwiftUIController.view)
+        settingsSwiftUIController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            settingsSwiftUIController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            settingsSwiftUIController.view.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            settingsSwiftUIController.view.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            
+        ])
+    }
+    
+    
+    
+    
+    
 }
