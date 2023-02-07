1be2cef9fa7b32606270d9af3b7bf47531029525
Chirag Arora <arora2002chirag@gmail.com>
2023-02-07 20:12:33 +0530

    Search Request Builder

--- RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
@@ -20,26 +20,59 @@ final class RMSearchViewViewModel {
     
     
     private var optionMapUpdateBlock: (((RMSearchInputViewViewModel.DynamicOptions, String)) -> Void)?
-
     
-    
+    private var searchResulthandler: (() -> Void)?
     
     // MARK: - Init
     
     init(config: RMSearchViewController.Config) {
         self.config = config
     }
+    
 
     
     //MARK: - Public
     
+    public func registerSearchResultHandler(_ block: @escaping () -> Void) {
+        self.searchResulthandler = block
+    }
+    
     public func executeSearch() {
-        //Create request based on filters
-        // send API call
-        // Notify view of results, no results, error
+        // Test search text
+        searchText = "Rick"
+        
+        // Build arguments
+        var queryParams: [URLQueryItem] = [
+            URLQueryItem(name: "name", value: searchText)
+        ]
         
         
-         
+        
+        // Add Options
+        queryParams.append(contentsOf: optionMap.enumerated().compactMap({ _, element in
+            let key: RMSearchInputViewViewModel.DynamicOptions = element.key
+            let value: String = element.value
+            return URLQueryItem(name: key.queryArgument, value: value)
+            
+        }))
+    
+        // Create Request
+        let request = RMRequest(
+            endpoint: config.type.endpoint,
+            queryParameters: queryParams
+        )
+        
+        
+        RMService.shared.execute(request, expecting: RMGetAllCharactersResponse.self) { result in
+            // Notify view of results, no results, error
+            
+            switch result {
+            case .success(let model):
+                print("Search results found: \(model.results.count)")
+            case .failure:
+                break
+            }
+        }
     }
     
     public func set(query text: String) {
